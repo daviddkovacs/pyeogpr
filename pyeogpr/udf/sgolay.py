5357fb09d482278d839f505549e95e45337ced90
Dávid D.Kovács <123364246+daviddkovacs@users.noreply.github.com>
2024-07-19 11:35:30 +0200

    Add files via upload

---
+++ pyeogpr/udf/sgolay.py
@@ -0,0 +1,23 @@
+import openeo
+
+udf_sgolay = openeo.UDF("""
+
+import numpy as np
+import xarray as xr
+from scipy.signal import savgol_filter
+from openeo.udf import XarrayDataCube, inspect
+
+from pathlib import Path
+import os
+import sys
+
+init_xr = xr.DataArray()
+def apply_datacube(cube: XarrayDataCube, context: dict) -> XarrayDataCube:
+
+    array: xarray.DataArray = cube.get_array()
+    filled = array.interpolate_na(dim='t')
+    smoothed_array = savgol_filter(filled.values, 5, 2, axis=0)
+    return XarrayDataCube(
+        array=xarray.DataArray(smoothed_array, dims=array.dims, coords=array.coords)
+    )
+""")
