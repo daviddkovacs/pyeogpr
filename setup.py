099cd5fd075ec1a61ba041c5fc20d7ff2087ee98
Dávid D.Kovács <123364246+daviddkovacs@users.noreply.github.com>
2024-07-20 13:11:19 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(name="pyeogpr",
       version ="0.1",
-      description='Python based library to use Earth Observation data to retrieve biophysical maps using Gaussian Process Regression',
-      long_description="Using the openEO API the whole GPR machine learning process runs in the cloud",
+      description="Python based library to use Earth Observation data to retrieve biophysical maps using Gaussian Process Regression",
+      long_description=open('README.md', 'r').read(),
+      long_description_content_type='text/markdown',
       author = "Dávid D.Kovács",
       author_email = "daviddkovacs@gmail.com",
       packages = find_packages(include=["pyeogpr","pyeogpr.*"]),
