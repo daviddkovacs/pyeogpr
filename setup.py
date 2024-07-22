2257d1677423174b54c37eba7cc0517ad8a4974d
Dávid D.Kovács <koda@uv.es>
2024-07-22 21:58:32 +0200

    im lost by now

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name="pyeogpr",
-      version ="1.0.8",
+      version ="1.0.9",
       description="Python based library to use Earth Observation data to retrieve biophysical maps using Gaussian Process Regression",
       long_description=open('README.md', 'r').read(),
       long_description_content_type='text/markdown',
       author = "Dávid D.Kovács",
       author_email = "daviddkovacs@gmail.com",
-      packages = find_packages(where='src'),
+      packages = find_packages(),
       install_requires = ["numpy","openeo","scipy"])
