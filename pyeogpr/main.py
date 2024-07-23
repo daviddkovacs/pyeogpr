94f7e14a74b24d4be3ea9f82dac58fc024e2fc35
Dávid D.Kovács <koda@uv.es>
2024-07-23 20:12:29 +0200

    updated main functions

--- pyeogpr/main.py
+++ pyeogpr/main.py
@@ -8,11 +8,42 @@ from pyeogpr.udfsgolay import udf_sgolay
 
 class Datacube:
     
-    def __init__(self,sensor:str, bounding_box: list, temporal_extent: list, cloudmask = False):
+    def __init__(self,sensor:str, biovar: str bounding_box: list, temporal_extent: list, cloudmask = False):
+        """
         
+
+        Parameters
+        ----------
+        sensor : "SENTINEL2_L1C", "SENTINEL2_L2A", "SENTINEL3_OLCI_L1B"
+            Satellite's sensor to process the data.
+                        
+        biovar : Biophysical variable to process. The selected variable's map will be retrieved.
+        
+            Currently "built-in" variables available for each sensor:
+            
+                - SENTINEL2_L1C: "Cab","Cm","Cw","FVC","LAI","laiCab","laiCm","laiCw"
+                - SENTINEL2_L2A: "Cab","Cm","Cw","FVC","LAI","laiCab","laiCm","laiCw","CNC_Cab","CNC_Cprot"
+                - SENTINEL3_OLCI_L1B: "FAPAR","FVC","LAI","LCC"
+                
+        bounding_box : list
+            Your region of interest. Insert bbox as list. Can be selected from https://geojson.io/
+        
+        temporal_extent : list
+            Your temporal extent to be processed.
+        
+        cloudmask : Boolean
+            If "True" the Sentinel 2 cloud mask will be applied, with Gaussian convolution to have
+            smoother edges when masking.
+
+        Returns
+        -------
+        Datacube object that will be passed to openEO backend.
+
+        """
         self.connection = openeo.connect("https://openeo.dataspace.copernicus.eu").authenticate_oidc()
         print("""\n\n""")
         self.sensor = sensor
+        self.biovar = biovar
         self.bounding_box = bounding_box
         self.temporal_extent = temporal_extent
         self.cloudmask = cloudmask
@@ -26,28 +57,21 @@ class Datacube:
         self.gpr_cube_gapfilled = None
 
         
-    def construct_datacube(self, composite=None, method=None):
+    def construct_datacube(self, composite=None):
         """
         
 
         Parameters
         ----------
-        composite : TYPE, string
-            Compositing temporal interval. The resulting maps will have the following temporal steps use: "hour","day","dekad","week","season","month","year"
-            for more information visit: https://processes.openeo.org/#aggregate_temporal_period
+        composite : "hour","day","dekad","week","season","month","year"
+            Compositing temporal interval. The resulting maps will have the following temporal steps. 
+            For more information: https://processes.openeo.org/#aggregate_temporal_period
         
-        method : TYPE, string
-            Type of reducer to use when aggregating temporal periods. A reducer to be applied for the values contained in each period. 
-            A reducer is a single process such as mean or a set of processes, which computes a single value for a list of values, see the category 'reducer' for such processes.
 
-        Raises
-        ------
-        Exception
-            DESCRIPTION.
 
         Returns
         -------
-        Datacube object containing user defined parameters. This will be processed into an openEO datacube.
+        Datacube object containing user defined parameters and temporal compositing. This will be processed into an openEO datacube.
 
         """
         if self.sensor not in self.sensors_dict.keys():
@@ -74,8 +98,8 @@ class Datacube:
 
             if composite != None:
                 
-                self.masked_data = self.data.aggregate_temporal_period(composite,method).mask(mask)
-                print(f"Cloud masked, temporally composited datacube constructed: {composite} by {method} values.")
+                self.masked_data = self.data.aggregate_temporal_period(composite,"mean").mask(mask)
+                print(f"Cloud masked, temporally composited datacube constructed: {composite} by mean values.")
                 
             elif composite == None:
                 
@@ -86,8 +110,8 @@ class Datacube:
             
             if composite != None:
                 
-                self.masked_data = self.data.aggregate_temporal_period(composite,method)
-                print(f"Temporally composited datacube constructed: {composite} by {method} values. ")
+                self.masked_data = self.data.aggregate_temporal_period(composite,"mean")
+                print(f"Temporally composited datacube constructed: {composite} by mean values. ")
                 
             elif composite == None:
                 
@@ -97,7 +121,7 @@ class Datacube:
         else:
             print(f"{self.sensor} can't be masked")
     
-    def process_map(self, biovar, gapfill = False):
+    def process_map(self, gapfill = False):
         """
         
 
@@ -105,20 +129,21 @@ class Datacube:
         ----------
         biovar : string
             Biophysical varaiable to process. Check the available ones for your sensor/satellite of choice: https://pypi.org/project/pyeogpr/
-        gapfill : Boolen, optional
-            Set to "True" if you want to apply Savitzy Golay interpolator for cloud induced gap filling
+        gapfill : type, e.g. "Sgolay"
+            To apply Savitzy Golay interpolator for cloud-induced gap filling
 
         Returns
         -------
-        Starts the openEO based GPR processing of the biophsycal maps.
+        Starts the openEO based GPR processing of the biophsycal maps. You will need to open the openEO
+        Web Editor to download your maps.
 
         """
-        if biovar not in self.sensors_dict[self.sensor]["sensor_biovar"]:
-            raise Exception(f"'{biovar}' not available for this satellite/sensor. Please select from: " +  str(self.sensors_dict[self.sensor]["sensor_biovar"]))
+        if self.biovar not in self.sensors_dict[self.sensor]["sensor_biovar"]:
+            raise Exception(f"'{self.biovar}' not available for this satellite/sensor. Please select from: " +  str(self.sensors_dict[self.sensor]["sensor_biovar"]))
         
-        print(f"Processing {self.sensor} based {biovar}")
+        print(f"Processing {self.sensor} based {self.biovar}")
         
-        context = {"sensor": self.sensor,"biovar":biovar}
+        context = {"sensor": self.sensor,"biovar":self.biovar}
         
         self.gpr_cube = self.masked_data.apply_dimension(process=udf_gpr,
                                                          dimension="bands",
@@ -127,7 +152,7 @@ class Datacube:
         if gapfill == False:
             
             print("""----\nTo check the progress login to your openEO editor:\nhttps://openeo.dataspace.copernicus.eu/\n----""")
-            self.gpr_cube.execute_batch(title=f"{self.sensor}_{biovar}",outputfile=f"{self.sensor}_{biovar}.nc",
+            self.gpr_cube.execute_batch(title=f"{self.sensor}_{self.biovar}",outputfile=f"{self.sensor}_{self.biovar}.nc",
                                         job_options = {'executor-memory': '10g','udf-dependency-archives': 
                                                        ['https://github.com/daviddkovacs/pyeogpr/raw/main/models/GPR_models_bulk.zip#tmp/venv']})
         #TODO: implement Whittaker smoother
@@ -135,7 +160,7 @@ class Datacube:
             print("Smoother: Savitzky-Golay")
             self.gpr_cube_gapfilled = self.gpr_cube.apply_dimension(process=udf_sgolay, dimension="t")
 
-            self.gpr_cube_gapfilled.execute_batch(title=f"{self.sensor} {biovar} {gapfill}", outputfile=f"{self.sensor}_{biovar}_GF.nc",
+            self.gpr_cube_gapfilled.execute_batch(title=f"{self.sensor} {self.biovar} {gapfill}", outputfile=f"{self.sensor}_{self.biovar}_GF.nc",
                                                   job_options={'executor-memory': '10g', 'udf-dependency-archives': 
                                                                 ['https://github.com/daviddkovacs/pyeogpr/raw/main/models/GPR_models_bulk.zip#tmp/venv']})
         else:
