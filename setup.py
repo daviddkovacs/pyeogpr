d38a92a2720edf13bcc1605c6a5fba3f9c3e0bd1
Dávid D.Kovács <koda@uv.es>
2024-09-20 13:12:19 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="pyeogpr",
-      version ="1.3.2",
+      version ="1.3.4",
       description="Python based library to use Earth Observation data to retrieve biophysical maps using Gaussian Process Regression",
       long_description=open('README.md', 'r').read(),
       long_description_content_type='text/markdown',
